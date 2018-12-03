5916b97ce07718ae69eb79138f23aa2a5766226f
fvkramer <krame490@gmail.com>
2018-12-03 13:54:53 -0800

    Initialize webpack with React/Redux for TodoApp

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const greeting = <h1>Todos App</h1>;
+  ReactDOM.render(greeting, document.getElementById("content"));
+});
