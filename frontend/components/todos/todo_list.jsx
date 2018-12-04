bff0bbd304ac2e4a5c49a06a50a4e5f5d0e055cd
fvkramer <krame490@gmail.com>
2018-12-03 16:34:34 -0800

    Start to connect redux to react

---
+++ frontend/components/todos/todo_list.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+module.exports = () => <h3>Todo list goes here </h3>;
---
+++ frontend/components/todos/todo_list_container.jsx
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import TodoList from './todo_list';
+import allTodos from '../../reducers/selectors';
+
+const mapStateToProps = ({store}) => ({
+  todos: allTodos(store)
+}); 
+
+const mDP = (dispatch) => ({
+  recieveTodo: (todo) => dispatch(recieveTodo(todo))
+});
+
+const TodoListContainer = connect(mapStateToProps, mDP)(TodoList);
