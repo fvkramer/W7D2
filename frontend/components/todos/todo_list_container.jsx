c1b9e9c8674f0271a968264654f700c50fd38ae1
fvkramer <krame490@gmail.com>
2018-12-03 17:41:05 -0800

    final commit

--- frontend/components/todos/todo_list_container.jsx
+++ frontend/components/todos/todo_list_container.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import TodoList from './todo_list';
 import allTodos from '../../reducers/selectors';
 
-const mapStateToProps = ({store}) => ({
+const mapStateToProps = (store) => ({
   todos: allTodos(store)
 }); 
 
